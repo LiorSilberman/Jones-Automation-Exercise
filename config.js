7ae15d9fb060d9077b49dba3262f12980509bc2f
Lior Silberman <silberman1015@gmail.com>
2024-11-21 17:08:24 +0200

    Add config file contains constants

---
+++ config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    SELECTORS: {
+        nameInput: 'input[id="name"]',
+        emailInput: 'input[id="email"]',
+        phoneInput: 'input[id="phone"]',
+        companyInput: 'input[id="company"]',
+        dropdownOption: '#option3',
+        submitButton: 'button.primary.button',
+        successHeading: 'h1',
+    },
+    SCREENSHOT: {
+        before: 'before',
+        after: 'after',
+    },
+    FILES: {
+        data: './data',
+        screenshots: './screenshots',
+        dataCSV: 'data.csv',
+    },
+    SITE: 'https://testsite.getjones.com/ExampleForm/',
+};
